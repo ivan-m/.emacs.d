137e9bc8a72924bc0039400e56bbf75c7c71229e
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2014-01-01 02:24:38 +1100

    Re-do haskell hooks
    Ignore-this: 308c312fa8d867031de1119001c22f16
    
    Split out adding key bindings to mode-maps, etc.
    
    darcs-hash:20131231152438-9c7ab-f4ebe29d7a715958b3f3dc84a607754f2181e74c

--- site-lisp/haskell-settings.el
+++ site-lisp/haskell-settings.el
@@ -12,8 +12,8 @@
 ;;     (cache)))
 
 (add-hook 'haskell-mode-hook 'haskell-hook)
-(add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
-(add-hook 'haskell-interactive-mode-hook 'haskell-interactive-hook)
+;(add-hook 'haskell-cabal-mode-hook 'haskell-cabal-hook)
+;(add-hook 'haskell-interactive-mode-hook 'haskell-interactive-hook)
 
 (defun haskell-hook ()
   ; Smart indentation
@@ -22,88 +22,21 @@
   ;(define-key haskell-mode-map (kbd "<return>") 'haskell-simple-indent-newline-same-col)
   ;(define-key haskell-mode-map (kbd "C-<return>") 'haskell-simple-indent-newline-indent)
 
-  (turn-on-haskell-decl-scan)
-
-  ; (turn-on-haskell-unicode-input-method)
-
-  ; For camelCase
   (capitalized-words-mode)
 
-  ;; Load the current file (and make a session if not already made).
-  (define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
-  (define-key haskell-mode-map [?\C-c ?\C-r] 'haskell-process-reload-file)
-  (define-key haskell-mode-map [f5] 'haskell-process-load-or-reload)
+  (turn-on-haskell-decl-scan)
 
-  ;; Switch to the REPL.
-  (define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
-  ;; “Bring” the REPL, hiding all other windows apart from the source
-  ;; and the REPL.
-  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (ghc-init)
 
-
-  ;; Build the Cabal project.
-  (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  ;; Interactively choose the Cabal command to run.
-  (define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
-
-  ;; Get the type and info of the symbol at point, print it in the
-  ;; message buffer.
-  (define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
-  (define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
-
-  ;; Contextually do clever things on the space key, in particular:
-  ;;   1. Complete imports, letting you choose the module name.
-  ;;   2. Show the type of the symbol after the space.
-  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
-
-  ;; Jump to the imports. Keep tapping to jump between import
-  ;; groups. C-u f8 to jump back again.
-  (define-key haskell-mode-map [f8] 'haskell-navigate-imports)
-
-  ;; Jump to the definition of the current symbol.
-  (define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
-
-  ;; Move the code below the current nesting left one.
-  (define-key haskell-mode-map (kbd "M-<left>")
-    (lambda () (interactive) (haskell-move-nested -1)))
-
-  ;; Move the code below the current nesting right one.
-  (define-key haskell-mode-map (kbd "M-<right>")
-    (lambda () (interactive) (haskell-move-nested 1)))
-
-  (define-key haskell-mode-map (kbd "C-c C-s") 'toggle-scc-at-point)
-
-  (define-key haskell-mode-map (kbd "C-c l") 'hs-lint)
-
-  (define-key haskell-mode-map (kbd "C-c C-y") 'cabal-toggle-sandboxing-local)
-
-  (lambda () (font-lock-add-keywords nil
-                                     '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face prepend))))
+  (font-lock-add-keywords nil
+                          '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face prepend)))
 
   (flyspell-prog-mode)
-
-  ; (lambda () (setq process-connection-type nil))
-
-  (lambda () (ghc-init))
 
   ;(setq ac-sources '(ac-source-words-in-same-mode-buffers
   ;                   ac-source-dictionary
   ;                   ac-source-ghc-mod))
   )
-
-;; Useful to have these keybindings for .cabal files, too.
-(defun haskell-cabal-hook ()
-  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
-  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
-  (define-key haskell-cabal-mode-map (kbd "C-c C-l") 'switch-to-haskell))
-
-(defun haskell-interactive-hook ()
-  ; Don't use C-c c or C-c C-c so that computations in ghci can still be killed.
-  (define-key haskell-interactive-mode-map (kbd "C-z C-c") 'haskell-process-cabal-build)
-  (define-key haskell-interactive-mode-map (kbd "C-z c") 'haskell-process-cabal)
-  (define-key haskell-interactive-mode-map (kbd "C-c C-l") 'switch-to-haskell))
 
 ; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -148,6 +81,50 @@ point."
                (> (match-end 1) old-point))
           (kill-region (match-beginning 0) (match-end 0))
         (error "No SCC at point")))))
+
+; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+; https://github.com/chrisdone/chrisdone-emacs/blob/master/config/haskell.el
+
+(defun haskell-insert-doc ()
+  "Insert the documentation syntax."
+  (interactive)
+  (insert "-- | "))
+
+(defun haskell-insert-undefined ()
+  "Insert undefined."
+  (interactive)
+  (if (and (boundp 'structured-haskell-mode)
+           structured-haskell-mode)
+      (shm-insert-string "undefined")
+    (insert "undefined")))
+
+(defun haskell-move-right ()
+  (interactive)
+  (haskell-move-nested 1))
+
+(defun haskell-move-left ()
+  (interactive)
+  (haskell-move-nested -1))
+
+(defun haskell-who-calls (&optional prompt)
+  "Grep the codebase to see who uses the symbol at point."
+  (interactive "P")
+  (let ((sym (if prompt
+                 (read-from-minibuffer "Look for: ")
+               (haskell-ident-at-point))))
+    (let ((existing (get-buffer "*who-calls*")))
+      (when existing
+        (kill-buffer existing)))
+    (let ((buffer
+           (grep-find (format "cd %s && find . -name '*.hs' -exec grep -inH -e %s {} +"
+                              (haskell-session-current-dir (haskell-session))
+                              sym))))
+      (with-current-buffer buffer
+        (rename-buffer "*who-calls*")
+        (switch-to-buffer-other-window buffer)))))
+
+
 
 ; code block support in latex haskell
 
@@ -263,3 +240,64 @@ point."
                         (if cabal-use-sandbox
                             'cabal-repl
                           'ghci)))))
+; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+; Keybindings
+
+
+(define-key haskell-mode-map [?\C-c ?\C-l] 'haskell-process-load-file)
+(define-key haskell-mode-map [?\C-c ?\C-r] 'haskell-process-reload-file)
+(define-key haskell-mode-map [f5] 'haskell-process-load-or-reload)
+
+;; Switch to the REPL.
+(define-key haskell-mode-map [?\C-c ?\C-z] 'haskell-interactive-switch)
+;; "Bring" the REPL, hiding all other windows apart from the source
+;; and the REPL.
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+
+;; Build the Cabal project.
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+;; Interactively choose the Cabal command to run.
+(define-key haskell-mode-map (kbd "C-c c") 'haskell-process-cabal)
+
+;; Get the type and info of the symbol at point, print it in the
+;; message buffer.
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+
+;; Contextually do clever things on the space key, in particular:
+;;   1. Complete imports, letting you choose the module name.
+;;   2. Show the type of the symbol after the space.
+(define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)
+
+;; Jump to the imports. Keep tapping to jump between import
+;; groups. C-u f8 to jump back again.
+(define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+
+;; Jump to the definition of the current symbol.
+(define-key haskell-mode-map (kbd "M-.") 'haskell-mode-tag-find)
+
+(define-key haskell-mode-map (kbd "M-,") 'haskell-who-calls)
+
+;; Move the code below the current nesting left one.
+(define-key haskell-mode-map (kbd "M-<left>") 'haskell-move-left)
+
+;; Move the code below the current nesting right one.
+(define-key haskell-mode-map (kbd "M-<right>") 'haskell-move-right)
+
+(define-key haskell-mode-map (kbd "C-c C-s") 'toggle-scc-at-point)
+(define-key haskell-mode-map (kbd "C-c l") 'hs-lint)
+(define-key haskell-mode-map (kbd "C-c C-y") 'cabal-toggle-sandboxing-local)
+(define-key haskell-mode-map (kbd "C-c C-u") 'haskell-insert-undefined)
+
+
+; Don't use C-c c or C-c C-c so that computations in ghci can still be killed.
+(define-key haskell-interactive-mode-map (kbd "C-z C-c") 'haskell-process-cabal-build)
+(define-key haskell-interactive-mode-map (kbd "C-z c") 'haskell-process-cabal)
+;(define-key haskell-interactive-mode-map (kbd "C-c C-l") 'switch-to-haskell)
+
+
+(define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
+(define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+;(define-key haskell-cabal-mode-map (kbd "C-c C-l") 'switch-to-haskell)
