24654f109625acb8f801c343c131611039cc0309
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2014-01-02 23:07:04 +1100

    Remove (commented-out) manual windmove bindings
    Ignore-this: 73aabdaebc0c5c6ae1090bc13c265d8c
    
    darcs-hash:20140102120704-9c7ab-6abe66ba1d4889f5d5fdac68d30ecc40ee3c86b4

--- site-lisp/settings.el
+++ site-lisp/settings.el
@@ -36,11 +36,6 @@ specs, then the override spec."
 
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
-; Use prefixed window movement keys to avoid clashes
-;; (global-set-key (kbd "C-c <left>")  'windmove-left)
-;; (global-set-key (kbd "C-c <right>") 'windmove-right)
-;; (global-set-key (kbd "C-c <up>")    'windmove-up)
-;; (global-set-key (kbd "C-c <down>")  'windmove-down)
 
 ;; define function to shutdown emacs server instance
 (defun server-shutdown ()
