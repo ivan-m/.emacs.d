f28074aa81effed5e97c27d4fea77480aa9a8f20
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2015-02-12 21:36:38 +1100

    Use scc functions now defined in haskell-mode
    Ignore-this: e60a39817a0a571bbf7b719314bf4e09
    
    darcs-hash:20150212103638-9c7ab-cdd7fe0ecd139611e73f51bb9171464f399b339e

--- site-lisp/haskell-settings.el
+++ site-lisp/haskell-settings.el
@@ -81,47 +81,13 @@
 
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-;; Based upon http://www.serpentine.com/blog/2007/10/09/using-emacs-to-insert-scc-annotations-in-haskell-code/
-
 (defun toggle-scc-at-point (&optional arg)
   "Insert or kill (with universal-argument) an SCC annotation at
 point."
   (interactive "P")
   (if (equal arg nil)
-      (insert-scc-at-point)
-    (kill-scc-at-point)))
-
-(defun insert-scc-at-point ()
-  "Insert an SCC annotation at point."
-  (interactive)
-  (if (or (looking-at "\\b\\|[ \t]\\|$") (and (not (bolp))
-                                              (save-excursion
-                                                (forward-char -1)
-                                                (looking-at "\\b\\|[ \t]"))))
-      (let ((space-at-point (looking-at "[ \t]")))
-        (unless (and (not (bolp)) (save-excursion
-                                    (forward-char -1)
-                                    (looking-at "[ \t]")))
-          (insert " "))
-        (insert "{-# SCC \"\" #-}")
-        (unless space-at-point
-          (insert " "))
-        (forward-char (if space-at-point -5 -6)))
-    (error "Not over an area of whitespace")))
-
-(defun kill-scc-at-point ()
-  "Kill the SCC annotation at point."
-  (interactive)
-  (save-excursion
-    (let ((old-point (point))
-          (scc "\\({-#[ \t]*SCC \"[^\"]*\"[ \t]*#-}\\)[ \t]*"))
-      (while (not (or (looking-at scc) (bolp)))
-        (forward-char -1))
-      (if (and (looking-at scc)
-               (<= (match-beginning 1) old-point)
-               (> (match-end 1) old-point))
-          (kill-region (match-beginning 0) (match-end 0))
-        (error "No SCC at point")))))
+      (haskell-mode-insert-scc-at-point)
+    (haskell-mode-kill-scc-at-point)))
 
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
