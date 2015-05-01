6e8e2e90dbefea17558b25833528e86317767d73
ivan.miljenovic <ivan.miljenovic@gmail.com>
2015-05-01 10:31:52 +1000

    Magit <-> GitHub integration
    Ignore-this: 2e3be149db07f95a19cb5ce8475a49
    
    darcs-hash:20150501003152-d9f09-4f377479a79b74714fe05f926eefac4e13b612b9

--- site-lisp/settings.el
+++ site-lisp/settings.el
@@ -99,6 +99,40 @@
 (add-hook 'magit-mode-hook 'turn-on-magit-gh-pulls)
 (setq magit-last-seen-setup-instructions "1.4.0")
 
+;; http://endlessparentheses.com/automatically-configure-magit-to-access-github-prs.html
+(defun endless/add-PR-fetch ()
+  "If refs/pull is not defined on a GH repo, define it."
+  (let ((fetch-address
+         "+refs/pull/*/head:refs/pull/origin/*")
+        (magit-remotes
+         (magit-get-all "remote" "origin" "fetch")))
+    (unless (or (not magit-remotes)
+                (member fetch-address magit-remotes))
+      (when (string-match
+             "github" (magit-get "remote" "origin" "url"))
+        (magit-git-string
+         "config" "--add" "remote.origin.fetch"
+         fetch-address)))))
+
+(add-hook 'magit-mode-hook #'endless/add-PR-fetch)
+
+;; http://endlessparentheses.com/easily-create-github-prs-from-magit.html?source=rss
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/compare/%s"
+     (replace-regexp-in-string
+      "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+      (magit-get "remote"
+                 (magit-get-current-remote)
+                 "url"))
+     (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "V"
+     #'endless/visit-pull-request-url))
+
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ;; Global settings
 
