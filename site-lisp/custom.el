ccf71ab96fcbc3787f016be98dc4658564b67258
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2015-08-28 11:44:41 +1000

    Fix ido for .DS_Store files
    Wasn't listing any files in directories.

--- site-lisp/custom.el
+++ site-lisp/custom.el
@@ -92,7 +92,7 @@
  '(ido-ignore-directories (quote ("\\`CVS/" "\\`\\.\\./" "\\`\\./" "_darcs")))
  '(ido-ignore-files
    (quote
-    ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "_darcs/" "\\.DS_Store")))
+    ("\\`CVS/" "\\`#" "\\`.#" "\\`\\.\\./" "\\`\\./" "_darcs/" "\\`\\.DS_Store")))
  '(ido-max-work-file-list 100)
  '(ido-mode (quote both) nil (ido))
  '(ido-read-file-name-non-ido (quote (TeX-master-file-ask)))
