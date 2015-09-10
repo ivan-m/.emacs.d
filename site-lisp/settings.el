3e174733faa5793f1d3668600a2bb98fa20e1461
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2015-09-10 16:43:16 +1000

    Add timestamps to *Messages*

--- site-lisp/settings.el
+++ site-lisp/settings.el
@@ -317,6 +317,28 @@ the actual manpage using the function `man'."
 ;; (setq-default ps-print-header nil)
 ;; (set-variable 'lpr-switches nil)
 
+;; From http://www.emacswiki.org/emacs/DebugMessages
+
+(defadvice message (before who-said-that activate)
+  "Find out who said that thing. and say so."
+  (let ((trace nil) (n 1) (frame nil))
+    (while (setq frame (backtrace-frame n))
+      (setq n     (1+ n)
+            trace (cons (cadr frame) trace)) )
+    (ad-set-arg 0 (concat "<<%S>>:\n" (ad-get-arg 0)))
+    (ad-set-args 1 (cons trace (ad-get-args 1))) ))
+
+(ad-disable-advice 'message 'before 'who-said-that)
+(ad-update 'message)
+
+(defadvice message (before when-was-that activate)
+  "Add timestamps to `message' output."
+  (ad-set-arg 0 (concat (format-time-string "[%Y-%m-%d %T %Z] ")
+                        (ad-get-arg 0)) ))
+
+;; (ad-disable-advice 'message 'before 'when-was-that)
+;; (ad-update 'message)
+
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ;; scratch buffer
 
