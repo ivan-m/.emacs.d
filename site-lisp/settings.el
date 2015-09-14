5048c941d8be468ad0555aa8b803b199f38d9692
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2015-09-14 15:50:38 +1000

    Stop windmove errors

--- site-lisp/settings.el
+++ site-lisp/settings.el
@@ -42,7 +42,19 @@
       inhibit-startup-echo-area-message t)
 
 (when (fboundp 'windmove-default-keybindings)
-  (windmove-default-keybindings))
+  (global-set-key [s-left] (ignore-error-wrapper 'windmove-left))
+  (global-set-key [s-right] (ignore-error-wrapper 'windmove-right))
+  (global-set-key [s-up] (ignore-error-wrapper 'windmove-up))
+  (global-set-key [s-down] (ignore-error-wrapper 'windmove-down)))
+
+(defun ignore-error-wrapper (fn)
+  "Funtion return new function that ignore errors.
+   The function wraps a function with `ignore-errors' macro."
+  (lexical-let ((fn fn))
+    (lambda ()
+      (interactive)
+      (ignore-errors
+        (funcall fn)))))
 
 ;; define function to shutdown emacs server instance
 (defun server-shutdown ()
