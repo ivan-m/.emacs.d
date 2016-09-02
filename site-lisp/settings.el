0d0fe2532c32fe8343ea7f4cbec1ebb75135415f
Ivan Lazar Miljenovic <ivan.miljenovic@gmail.com>
2016-09-02 22:37:42 +1000

    Better tramp settings

--- site-lisp/settings.el
+++ site-lisp/settings.el
@@ -129,9 +129,14 @@
 (load "sql-settings")
 (load "orgmode-settings")
 
-(require 'tramp)
-(defun tramp-set-auto-save ()
-  (auto-save-mode -1))
+(eval-after-load 'tramp
+  '(setq backup-enable-predicate
+         (lambda (name)
+           (and (normal-backup-enable-predicate name)
+                (not
+                 (let ((method (file-remote-p name 'method)))
+                   (when (stringp method)
+                     (member method '("su" "sudo")))))))))
 
 ;; Doesn't seem to like being customized.
 (require 'recentf)
