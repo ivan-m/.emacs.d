ab09cb32853065e4d7cb64f7ebd9369505c3a237
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-03-02 12:05:50 +1100

    Prepare initialisation to use req-package

--- init.el
+++ init.el
@@ -7,6 +7,9 @@
 (setq custom-theme-directory
       (expand-file-name "themes" site-lisp-dir))
 
+;; Keep emacs Custom-settings in separate file
+(setq custom-file (expand-file-name "custom.el" site-lisp-dir))
+
 ;; Set up load path
 (add-to-list 'load-path site-lisp-dir)
 
@@ -19,109 +22,128 @@
 ;; depends on some packages being available/loaded.
 
 (require 'package)
-
-;; In case we want to only load a sub-set.
-;; (setq package-load-list
-;;       '((fill-column-indicator t)))
-
 (setq package-enable-at-startup nil)
+
+;; I don't want package-selected-packages saved anywhere, so re-define
+;; this function to prevent it.
+(defun package--save-selected-packages (&rest opt) nil)
+
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "https://melpa.org/packages/")
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
-(when (boundp 'package-pinned-packages)
-  (setq package-pinned-packages
-        '((ensime . "melpa-stable"))))
-
 (package-initialize)
-
-;; I don't want package-selected-packages saved anywhere, so re-define
-;; this function to prevent it.
-(defun package--save-selected-packages (&rest opt) nil)
 
 (when (not package-archive-contents)
   (package-refresh-contents))
 
-(defvar my-packages
-  '(alect-themes
-    auctex
-    auto-complete
-    auto-highlight-symbol
-    auto-package-update
-    bbdb
-    bug-reference-github
-    bury-successful-compilation
-    cider
-    clojure-cheatsheet
-    company
-    company-cabal
-    company-ghci
-    crux
-    csv-mode
-    darcsum
-    desktop
-    diff-hl
-    diminish
-    diredful
-    dynamic-fonts
-    ebal
-    exec-path-from-shell
-    flycheck-color-mode-line
-    flycheck-haskell
-    flycheck-ledger
-    ghc
-    github-notifier
-    goto-chg
-    graphviz-dot-mode
-    haskell-mode
-    helm
-    hindent
-    htmlize
-    ido-at-point
-    ido-ubiquitous
-    imenu-anywhere ;; uses ido, works better than idomenu
-    ledger-mode
-    lorem-ipsum
-    magit
-    magit-filenotify
-    markdown-mode
-    markdown-mode+
-    mmm-mode
-    nix-mode
-    nixos-options
-    org
-    org-pandoc
-    outline-magic
-    pandoc-mode
-    paradox
-    paredit
-    paredit-menu
-    rainbow-delimiters
-    rw-hunspell
-    rw-ispell
-    rw-language-and-country-codes
-    shm
-    smex
-    sql-indent
-    sqlup-mode
-    typed-clojure-mode
-    unicode-fonts
-    unkillable-scratch
-    yaml-mode
-    zoom-frm
-    )
-  )
-
-;; If a package isn't already installed (but is available), install it.
-(dolist (p my-packages)
-  (when (and (not (package-installed-p p))
-             (assoc p package-archive-contents))
+(defun try-install (package)
+  "Try and install the specified package manually; die if it fails"
+  (when (and (not (package-installed-p package))
+             (assoc package package-archive-contents))
     (condition-case err
-        (package-install p)
+        (package-install package)
       (error
        (message "%s" (error-message-string err))))))
+
+(try-install 'req-package)
+(require 'req-package)
+
+;; Load all packages from site-lisp-dir
+(req-package load-dir
+  :force true
+  :init
+  (setq force-load-messages nil)
+  (setq load-dir-debug nil)
+  (setq load-dir-recursive t)
+  :config
+  (load-dir-one site-lisp-dir)
+  (req-package-finish))
+
+;; (when (boundp 'package-pinned-packages)
+;;   (setq package-pinned-packages
+;;         '((ensime . "melpa-stable"))))
+
+;; (defvar my-packages
+;;   '(alect-themes
+;;     auctex
+;;     auto-complete
+;;     auto-highlight-symbol
+;;     auto-package-update
+;;     bbdb
+;;     bug-reference-github
+;;     bury-successful-compilation
+;;     cider
+;;     clojure-cheatsheet
+;;     company
+;;     company-cabal
+;;     company-ghci
+;;     crux
+;;     csv-mode
+;;     darcsum
+;;     desktop
+;;     diff-hl
+;;     diminish
+;;     diredful
+;;     dynamic-fonts
+;;     ebal
+;;     exec-path-from-shell
+;;     flycheck-color-mode-line
+;;     flycheck-haskell
+;;     flycheck-ledger
+;;     ghc
+;;     github-notifier
+;;     goto-chg
+;;     graphviz-dot-mode
+;;     haskell-mode
+;;     helm
+;;     hindent
+;;     htmlize
+;;     ido-at-point
+;;     ido-ubiquitous
+;;     imenu-anywhere ;; uses ido, works better than idomenu
+;;     ledger-mode
+;;     lorem-ipsum
+;;     magit
+;;     magit-filenotify
+;;     markdown-mode
+;;     markdown-mode+
+;;     mmm-mode
+;;     nix-mode
+;;     nixos-options
+;;     org
+;;     org-pandoc
+;;     outline-magic
+;;     pandoc-mode
+;;     paradox
+;;     paredit
+;;     paredit-menu
+;;     rainbow-delimiters
+;;     rw-hunspell
+;;     rw-ispell
+;;     rw-language-and-country-codes
+;;     shm
+;;     smex
+;;     sql-indent
+;;     sqlup-mode
+;;     typed-clojure-mode
+;;     unicode-fonts
+;;     unkillable-scratch
+;;     yaml-mode
+;;     zoom-frm
+;;     )
+;;   )
+
+;; ;; If a package isn't already installed (but is available), install it.
+;; (dolist (p my-packages)
+;;   (when (and (not (package-installed-p p))
+;;              (assoc p package-archive-contents))
+;;     (condition-case err
+;;         (package-install p)
+;;       (error
+;;        (message "%s" (error-message-string err))))))
 
 ;; (auto-package-update-maybe)
 
@@ -132,9 +154,6 @@
 
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ;; Actually get things going.
-
-;; Keep emacs Custom-settings in separate file
-(setq custom-file (expand-file-name "custom.el" site-lisp-dir))
 
 ;; Load this first as it needs to load items for settings, etc.
 (load "settings")
