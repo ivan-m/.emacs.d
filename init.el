c0af15964bd3c9626078a46594977991438ff27c
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-03-08 16:52:52 +1100

    Final cleanup

--- init.el
+++ init.el
@@ -13,6 +13,8 @@
 ;; Set up load path
 (add-to-list 'load-path site-lisp-dir)
 
+(load custom-file :noerror)
+
 (defun init-compile-dir (dir)
   ;; Byte-compile a directory when starting emacs.
   (with-no-warnings (byte-recompile-directory dir 0)))
@@ -64,114 +66,10 @@
   (load-dir-one site-lisp-dir)
   (req-package-finish))
 
-;; (when (boundp 'package-pinned-packages)
-;;   (setq package-pinned-packages
-;;         '((ensime . "melpa-stable"))))
-
-;; (defvar my-packages
-;;   '(alect-themes
-;;     auctex
-;;     auto-complete
-;;     auto-highlight-symbol
-;;     auto-package-update
-;;     bbdb
-;;     bug-reference-github
-;;     bury-successful-compilation
-;;     cider
-;;     clojure-cheatsheet
-;;     company
-;;     company-cabal
-;;     company-ghci
-;;     crux
-;;     csv-mode
-;;     darcsum
-;;     desktop
-;;     diff-hl
-;;     diminish
-;;     diredful
-;;     dynamic-fonts
-;;     ebal
-;;     exec-path-from-shell
-;;     flycheck-color-mode-line
-;;     flycheck-haskell
-;;     flycheck-ledger
-;;     ghc
-;;     github-notifier
-;;     goto-chg
-;;     graphviz-dot-mode
-;;     haskell-mode
-;;     helm
-;;     hindent
-;;     htmlize
-;;     ido-at-point
-;;     ido-ubiquitous
-;;     imenu-anywhere ;; uses ido, works better than idomenu
-;;     ledger-mode
-;;     lorem-ipsum
-;;     magit
-;;     magit-filenotify
-;;     markdown-mode
-;;     markdown-mode+
-;;     mmm-mode
-;;     nix-mode
-;;     nixos-options
-;;     org
-;;     org-pandoc
-;;     outline-magic
-;;     pandoc-mode
-;;     paradox
-;;     paredit
-;;     paredit-menu
-;;     rainbow-delimiters
-;;     rw-hunspell
-;;     rw-ispell
-;;     rw-language-and-country-codes
-;;     shm
-;;     smex
-;;     sql-indent
-;;     sqlup-mode
-;;     typed-clojure-mode
-;;     unicode-fonts
-;;     unkillable-scratch
-;;     yaml-mode
-;;     zoom-frm
-;;     )
-;;   )
-
-;; ;; If a package isn't already installed (but is available), install it.
-;; (dolist (p my-packages)
-;;   (when (and (not (package-installed-p p))
-;;              (assoc p package-archive-contents))
-;;     (condition-case err
-;;         (package-install p)
-;;       (error
-;;        (message "%s" (error-message-string err))))))
-
-;; (auto-package-update-maybe)
-
 ;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 ;; Byte-compile settings.
 
 (init-compile-dir site-lisp-dir)
-
-;; %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-;; Actually get things going.
-
-;; Load this first as it needs to load items for settings, etc.
-(load "settings")
-
-(load custom-file :noerror)
-
-;; When using emacs --daemon, it seems that the cursor color isn't
-;; set.  As such, use this to set it manually (needs to be set after
-;; customize).
-;;
-;; Unfortunately, there doesn't seem to be any way of making this work
-;; for all themes, and the theme-specific cursor color needs to be
-;; used.
-
-(add-to-list 'default-frame-alist
-             `(cursor-color . ,(alect-get-color 'dark 'cursor)))
 
 ;; Local Variables:
 ;; flycheck-disabled-checkers: (emacs-lisp-checkdoc)
--- site-lisp/common-settings.el
+++ site-lisp/common-settings.el
@@ -127,23 +127,29 @@
              (just-one-space 0)
              (backward-char 1))))
 
-(defadvice message (before who-said-that activate)
-  "Find out who said that thing. and say so."
-  (let ((trace nil) (n 1) (frame nil))
-    (while (setq frame (backtrace-frame n))
-      (setq n     (1+ n)
-            trace (cons (cadr frame) trace)) )
-    (ad-set-arg 0 (concat "<<%S>>:\n" (ad-get-arg 0)))
-    (ad-set-args 1 (cons trace (ad-get-args 1))) ))
-
+;; https://www.emacswiki.org/emacs/DebugMessages
+;;
+;; We don't want this enabled by default
+;;
+;; (defadvice message (before who-said-that activate)
+;;   "Find out who said that thing. and say so."
+;;   (let ((trace nil) (n 1) (frame nil))
+;;     (while (setq frame (backtrace-frame n))
+;;       (setq n     (1+ n)
+;;             trace (cons (cadr frame) trace)) )
+;;     (ad-set-arg 0 (concat "<<%S>>:\n" (ad-get-arg 0)))
+;;     (ad-set-args 1 (cons trace (ad-get-args 1))) ))
+;;
 ;; (ad-disable-advice 'message 'before 'who-said-that)
-(ad-update 'message)
+;; (ad-update 'message)
 
 (defadvice message (before when-was-that activate)
   "Add timestamps to `message' output."
   (ad-set-arg 0 (concat (format-time-string "[%Y-%m-%d %T %Z] ")
                         (ad-get-arg 0)) ))
 
+;; (ad-disable-advice 'message 'before 'when-was-that)
+;; (ad-update 'message)
 
 (defadvice Info-follow-nearest-node (around gitman activate)
   "When encountering a cross reference to the `gitman' info
