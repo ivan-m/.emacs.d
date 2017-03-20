5df160e9da82316f6046180f59db8eabb3113cb3
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-03-20 13:50:17 +1100

    Fix up loading orgmode

--- site-lisp/orgmode-settings.el
+++ site-lisp/orgmode-settings.el
@@ -3,7 +3,7 @@
 ;; Having problems? Try this: http://emacs.stackexchange.com/a/16616
 
 (req-package org
-  :require
+  :ensure
   org-plus-contrib
   :init
   (setq org-completion-use-ido t)
@@ -11,31 +11,6 @@
   (setq org-CUA-compatible t)
   ;; So C-k doesn't kill whole sub-trees.
   (setq org-ctrl-k-protect-subtree t)
-  :comands
-  turn-on-orgtbl
-  org-mode
-  orgtbl-mode
-  :mode
-  ("\\.org$" . org-mode)
-  :bind
-  (("C-c l" . org-store-link)
-   ("C-c a" . org-agenda)
-   ("C-c b" . org-iswitchb))
-  :config
-
-  (require 'org-element) ;; to get org-element--set-regexps
-  ;; From http://stackoverflow.com/questions/24169333/how-can-i-emphasize-or-verbatim-quote-a-comma-in-org-mode/24173780
-  (setcar (nthcdr 2 org-emphasis-regexp-components) " \t\r\n")
-  (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
-  ;;(custom-set-variables `(org-emphasis-alist ',org-emphasis-alist)) ;; doesn't seem to be needed
-  (org-element--set-regexps)
-
-  ;; Make windmove work in org-mode:
-  (add-hook 'org-shiftup-final-hook 'windmove-up)
-  (add-hook 'org-shiftleft-final-hook 'windmove-left)
-  (add-hook 'org-shiftdown-final-hook 'windmove-down)
-  (add-hook 'org-shiftright-final-hook 'windmove-right)
-
 
   ;; Font screws up in --daemon mode
   (defun org-column-view-uses-fixed-width-face ()
@@ -55,9 +30,38 @@
               (when (outline-invisible-p)
                 (save-excursion
                   (outline-previous-visible-heading 1)
-                  (org-show-subtree))))))
+                  (org-show-subtree)))))
+  :commands
+  org-mode
+  :mode
+  ("\\.org$" . org-mode)
+  :bind
+  (("C-c l" . org-store-link)
+   ("C-c a" . org-agenda)
+   ("C-c b" . org-iswitchb))
+  :config
 
-(req-package org-pandoc)
+  (require 'org-element) ;; to get org-element--set-regexps
+  ;; From http://stackoverflow.com/questions/24169333/how-can-i-emphasize-or-verbatim-quote-a-comma-in-org-mode/24173780
+  (setcar (nthcdr 2 org-emphasis-regexp-components) " \t\r\n")
+  (org-set-emph-re 'org-emphasis-regexp-components org-emphasis-regexp-components)
+  ;;(custom-set-variables `(org-emphasis-alist ',org-emphasis-alist)) ;; doesn't seem to be needed
+  (org-element--set-regexps)
+
+  ;; Make windmove work in org-mode:
+  (add-hook 'org-shiftup-final-hook 'windmove-up)
+  (add-hook 'org-shiftleft-final-hook 'windmove-left)
+  (add-hook 'org-shiftdown-final-hook 'windmove-down)
+  (add-hook 'org-shiftright-final-hook 'windmove-right))
+
+(req-package org-table
+  :ensure
+  org-plus-contrib
+  :commands
+  turn-on-orgtbl
+  orgtbl-mode)
+
+;; (req-package org-pandoc)
 
 (req-package outline-magic)
 
