eba6ac30b75c130cef6c1602a69b4fb18fef9226
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2017-04-04 16:40:30 +1000

    Scala improvements

--- site-lisp/scala-settings.el
+++ site-lisp/scala-settings.el
@@ -5,13 +5,19 @@
   ensime
   company
   :init
+  (setq scala-indent:align-parameters t)
+  (setq scala-indent:align-forms t)
+
   (add-hook 'scala-mode-hook
             (lambda ()
-              (company-mode)
+              (company-mode 1)
+              (flycheck-mode 1)
               (ensime-mode)
               (scala-mode:goto-start-of-code)))
   :commands
   scala-mode
+  :interpreter
+  ("scala" . scala-mode)
   :bind
   (:map scala-mode-map
    ("RET" . scala-mode-newline-comments))
@@ -23,7 +29,18 @@
     (newline-and-indent)
     (scala-indent:insert-asterisk-on-multiline-comment)))
 
-(req-package sbt-mode)
+(req-package sbt-mode
+  :commands
+  sbt-start
+  sbt-command
+  sbt-hydra
+  :config
+  ;; WORKAROUND: https://github.com/ensime/emacs-sbt-mode/issues/31
+  ;; allows using SPACE when in the minibuffer
+  (substitute-key-definition
+   'minibuffer-complete-word
+   'self-insert-command
+   minibuffer-local-completion-map))
 
 (req-package ensime
   ;; Needs to match tool version, so this is easier
